071207f16aade62e8c8ad4bf27aac5cc75d5a2a5
poketopa <lhs5427ll@gmail.com>
2025-10-26 20:36:47 +0900

    feat(parser): 자동차 이름, 시도 횟수 파싱 클래스 생성

--- src/main/java/racingcar/parser/InputParser.java
+++ src/main/java/racingcar/parser/InputParser.java
@@ -1,5 +1,14 @@
 package racingcar.parser;
 
-public class InputParser {
+import java.util.Arrays;
+import java.util.List;
 
+public class InputParser {
+    public List<String> parseCarNames(String rawCarNames) {
+        return Arrays.asList(rawCarNames.split(","));
+    }
+
+    public int parseTryCount(String rawTryCount) {
+        return Integer.parseInt(rawTryCount);
+    }
 }
