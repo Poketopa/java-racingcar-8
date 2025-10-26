07b3c4d540febfaeb9f82a263d2ac8e9a07740bd
poketopa <lhs5427ll@gmail.com>
2025-10-27 01:45:46 +0900

    feat(service): 경주 결과와 우승자 정보를 DTO에 저장하고 리턴하는 로직 생성

--- src/main/java/racingcar/service/RacingcarService.java
+++ src/main/java/racingcar/service/RacingcarService.java
@@ -1,18 +1,25 @@
 package racingcar.service;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
+import racingcar.dto.CarSnapshot;
+import racingcar.dto.RaceResult;
 import racingcar.dto.RacingcarRequest;
+import racingcar.dto.RoundSnapshot;
 import racingcar.model.Car;
 
 public class RacingcarService {
-    public void startRace(RacingcarRequest request) {
+    public RaceResult startRace(RacingcarRequest request) {
         List<Car> carList = createCar(request.carNames());
 
-        race(carList, request.tryCount());
-    }
+        List<RoundSnapshot> roundSnapshotList = race(carList, request.tryCount());
 
-    private void race(List<Car> carList, int tryCount) {
+        int longestDistance = getLongestDistance(carList);
+
+        List<Car> winnerList = findWinner(carList, longestDistance);
+
+        return new RaceResult(roundSnapshotList, winnerList);
     }
 
     private List<Car> createCar(List<String> carNames) {
@@ -22,6 +29,45 @@ public class RacingcarService {
             carList.add(car);
         }
         return carList;
+    }
+
+    private List<RoundSnapshot> race(List<Car> carList, int tryCount) {
+        List<RoundSnapshot> roundSnapshotList = new ArrayList<>();
+        for (int round = 1; round <= tryCount; round++) {
+            roundSnapshotList.add(new RoundSnapshot(round, getRandomNumberAndGo(carList)));
+        }
+        return roundSnapshotList;
+    }
+
+    private List<CarSnapshot> getRandomNumberAndGo(List<Car> carList) {
+        List<CarSnapshot> roundSnapshotList = new ArrayList<>();
+        for (int i = 0; i < carList.size(); i++) {
+            Car car = carList.get(i);
+            if (Randoms.pickNumberInRange(0, 9) >= 4) {
+                car.go();
+            }
+            CarSnapshot roundSnapshot = new CarSnapshot(car.getName(), car.getDistance());
+            roundSnapshotList.add(roundSnapshot);
+        }
+        return roundSnapshotList;
+    }
+
+    private int getLongestDistance(List<Car> carList) {
+        int longestDistance = Integer.MIN_VALUE;
+        for (Car car : carList) {
+            longestDistance = Math.max(longestDistance, car.getDistance());
+        }
+        return longestDistance;
+    }
+
+    private List<Car> findWinner(List<Car> carList, int longestDistance) {
+        List<Car> winnerList = new ArrayList<>();
+        for (Car car : carList) {
+            if (car.getDistance() == longestDistance) {
+                winnerList.add(car);
+            }
+        }
+        return winnerList;
     }
 
 
